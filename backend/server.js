a008dad524ea6951d57be938124be6e42e9411eb
creative-uk <rafay.creative786@gmail.com>
2024-10-28 13:32:13 +0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     credentials: true, // Note: Boolean value, not a string
-    origin: "https://pharmsaleb2b.netlify.app/",
+    origin: "https://pharmsaleb2b.netlify.app",
 }));
 
 
