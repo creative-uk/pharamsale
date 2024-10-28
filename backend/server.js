f7abfa3bbc608a1b4507caa206572c6581497f1c
creative-uk <rafay.creative786@gmail.com>
2024-10-28 15:29:55 +0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use(cors({
     credentials: true, // Note: Boolean value, not a string
 
-    origin: "http://localhost:5173",
+    origin: process.env.FOrigin,
 
 }));
 
